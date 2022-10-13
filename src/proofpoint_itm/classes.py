8d47e0b22730b9d41fe858b85b1732a3c93a757e
drizzo-tech <michael.olden@oldendigital.com>
2022-10-13 10:01:15 -0700

    Added additional class representations of objects
    Added as_dict() method to each class to allow
    calling only the needed keys for object creation
    and/or updates. Previously using .__dict__ included
    additional info not relevate to API data

--- src/proofpoint_itm/classes.py
+++ src/proofpoint_itm/classes.py
@@ -36,6 +36,24 @@ class Predicate:
                             self.refs.append(v)
                 elif k == '$and' or k == '$or':
                     self._get_nested_refs(v)
+    
+    def as_dict(self):
+        """
+        Returns a dict object with only the keys needed to create a new predicate
+        """
+        data = {
+            'definition': self.definition,
+            'patterns': self.patterns,
+            'predicates': self.predicates,
+            'purposes': self.purposes,
+            'lists': self.lists,
+            'tags': self.tags,
+            'details': self.details,
+            'alias': self.alias,
+            'risk': self.risk,
+            'kind': self.kind
+        }
+        return data
 
 
 class Rule:
@@ -56,6 +74,21 @@ class Rule:
         except Exception as e:
             print(f'Error creating rule object: {e}')
 
+    def as_dict(self):
+        """
+        Returns a dict object with only the keys needed to create a new rule
+        """
+        data = {
+            'predicate': self.predicate,
+            'actions': self.actions,
+            'options': self.options,
+            'tags': self.tags,
+            'details': self.details,
+            'alias': self.alias,
+            'status': self.status,
+            'kind': self.kind
+        }
+        return data
 
 class Tag:
     """
@@ -70,21 +103,54 @@ class Tag:
             self.details = data.get('details', {})
         except Exception as e:
             print(f'Error creating rule object: {e}')
+    
+    def as_dict(self):
+        """
+        Returns a dict object with only the keys needed to create a new tag
+        """
+        data = {
+            'status': self.status,
+            'name': self.name,
+            'alias': self.alias,
+            'details': self.details,
+        }
+        return data
 
-class Policy:
+class AgentPolicy:
     """
     An class representing a Proofpoint ITM Agent Policy
     """
     def __init__(self, json_data={}):
         data = copy.deepcopy(json_data)
         try:
-            self.policy = data.get('policy', {})
             self.alias = data.get('alias')
+            self.description = data.get('description')
             self.kind = data.get('kind')
-            self.settings = data.get('settings')
+            self.refs = data.get('refs', {'rules': {'rules': []}})
+            self.match = data.get('match', {})
+            self.settings = data.get('settings', {})
             self.encrypt = data.get('encrypt', False)
+            self.deleted = data.get('deleted', False)
+            self.priority = data.get('priority', '-1')
         except Exception as e:
-            print(f'Error creating rule object: {e}')
+            print(f'Error creating agent policy object: {e}')
+
+    def as_dict(self):
+        """
+        Returns a dict object with only the keys needed to create a new policy
+        """
+        data = {
+            'description': self.description,
+            'match': self.match,
+            'settings': self.settings,
+            'alias': self.alias,
+            'kind': self.kind,
+            'refs': self.refs,
+            'encrypt': self.encrypt,
+            'deleted': self.deleted,
+            'priority': self.priority
+        }
+        return data
 
 class TargetGroup:
     """
@@ -98,20 +164,172 @@ class TargetGroup:
             self.alias = data.get('alias')
             self.targets = data.get('targets', [])
             self.purposes = data.get('purposes', [])
+            self.extent = data.get('extent', 'tenant')
         except Exception as e:
             print(f'Error creating rule object: {e}')
 
+    def as_dict(self):
+        """
+        Returns a dict object with only the keys needed to create a new target-group
+        """
+        data = {
+            'displayName': self.displayName,
+            'description': self.description,
+            'targets': self.targets,
+            'purposes': self.purposes,
+            'alias': self.alias,
+            'extent': self.extent
+        }
+        return data
+
 class Target:
     """
-    An class representing a Proofpoint ITM notification target
+    A class representing a Proofpoint ITM notification target
     """
     def __init__(self, json_data={}):
         data = copy.deepcopy(json_data)
         try:
-            self.template = data.get('template', {})
             self.details = data.get('details', {})
             self.relations = data.get('relations', [])
             self.kind = data.get('kind')
             self.status = data.get('status')
         except Exception as e:
             print(f'Error creating rule object: {e}')
+
+        try:
+            self.template = data['template']
+        except KeyError:
+            pass
+
+    def as_dict(self):
+        """
+        Returns a dict object with only the keys needed to create a new target
+        """
+        data = {
+            'relations': self.relations,
+            'details': self.details,
+            'status': self.status,
+            'kind': self.kind
+        }
+        try:
+            data['template'] = self.template
+        except:
+            pass
+
+        return data
+
+class Dictionary:
+    """
+    A class representing a Proofpoint ITM dictionary
+    """
+    def __init__(self, json_data={}):
+        data = copy.deepcopy(json_data)
+        try:
+            self.name = data.get('name')
+            self.description = data.get('description')
+            self.entries = data.get('entries', [])
+        except Exception as e:
+            print(f'Error creating dictionary object: {e}')
+
+        try:
+            self.category = data['category']['id']
+        except KeyError:
+            print(f'Could not find category id for dictionary: {self.name}')
+    
+    def as_dict(self):
+        """
+        Returns a dict object with only the keys needed to create a new dictionary
+        """
+        data = {
+            'name': self.name,
+            'description': self.description,
+            'entries': self.entries
+        }
+        
+        try:
+            data['category'] = self.category
+        except:
+            pass
+        
+        return data
+
+class Detector:
+    """
+    A class representing a Proofpoint ITM detector
+    """
+    def __init__(self, json_data={}):
+        data = copy.deepcopy(json_data)
+        try:
+            self.name = data.get('name')
+            self.description = data.get('description')
+            self.expression = data.get('expression')
+            self.dictionaries = data.get('dictionaries', [])
+            self.smartIds = data.get('smartIds', [])
+        except Exception as e:
+            print(f'Error creating dictionary object: {e}')
+
+        try:
+            self.category = data['category']['id']
+        except KeyError:
+            print(f'Could not find category id for detector: {self.name}')
+
+    def as_dict(self):
+        """
+        Returns a dict object with only the keys needed to create a new detector
+        """
+        data = {
+            'name': self.name,
+            'description': self.description,
+            'expression': self.expression,
+            'dictionaries': self.dictionaries,
+            'smartIds': self.smartIds
+        }
+
+        try:
+            data['category'] = self.category
+        except:
+            pass
+
+        return data
+
+
+class DetectorSet:
+    """
+    A class representing a Proofpoint ITM detector set
+    """
+    def __init__(self, json_data={}):
+        data = copy.deepcopy(json_data)
+        try:
+            self.name = data.get('name')
+            self.description = data.get('description')
+            detectors = data.get('enabledDetectors', [])
+            enabledDetectors = []
+            for detector in detectors:
+                enabledDetectors.append(detector['id'])
+            self.enabledDetectors = enabledDetectors
+            #self.dictionaries = data.get('dictionaries', [])
+            #self.smartIds = data.get('smartIds', [])
+        except Exception as e:
+            print(f'Error creating dictionary object: {e}')
+
+        try:
+            self.category = data['category']['id']
+        except KeyError:
+            print(f'Could not find category id for detector: {self.name}')
+
+    def as_dict(self):
+        """
+        Returns a dict object with only the keys needed to create a new detector
+        """
+        data = {
+            'name': self.name,
+            'description': self.description,
+            'enabledDetectors': self.enabledDetectors,
+        }
+
+        try:
+            data['category'] = self.category
+        except:
+            pass
+
+        return data
