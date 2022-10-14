6aa6179e76174f17ec04110778171fe4a028d4f2
drizzo-tech <michael.olden@oldendigital.com>
2022-10-14 11:04:12 -0700

    updated sample script

--- sample_scripts/update_policy.py
+++ sample_scripts/update_policy.py
@@ -1,64 +1,52 @@
 import json
 import argparse
 from proofpoint_itm import ITMClient
+from proofpoint_itm.classes import AgentPolicy
 
 
 parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--settings", default="settings.json",
+                    help="Settings file")
 parser.add_argument('--file', '-f', type=str, required=True,
                     help='file name/path for the list of usernames to add to a policy')
 parser.add_argument('--policy', '-p', type=str, required=True,
                     help='The name of the policy to update')
-parser.add_argument('--action', '-a', choices=['update', 'overwrite'], default='update',
-                    help='Choose update or overwrite action, defaults to update')
 args = parser.parse_args()
 
-# read in the config settings
-with open('settings.json', 'r') as f:
+# read in the config settings and get client
+with open(args.settings, 'r') as f:
     settings = json.loads(f.read())
 
+itm_client = ITMClient(settings)
 
-itm_client = ITMClient(
-    settings['ITM']['base_url'],
-    settings['ITM']['client_id'],
-    client_secret=settings['ITM']['client_secret'])
-
-policy = itm_client.get_policies(params={'alias': args.policy})
-policy_id = policy['data'][0]['id']
+target_policy = itm_client.get_agent_policies(params={'alias': args.policy})
+target_policy_id = target_policy[0]['id']
+agentpolicy = AgentPolicy(target_policy[0]['policy'])
 
 # import data
 with open (args.file, 'r') as f:
     usernames = [u.strip() for u in f]
 
-# update policy
-if args.action == 'update':
-    # build web request body
-    data = {
-        'match': {
-            'simple': {
-                'rules': [
-                    {
-                        'user.username': usernames
-                    }
-                ]
-            }
+# build the match condition with username list
+match = {
+    "simple": {
+        "rules": [
+            {"user.username": usernames}
+        ]
+    },
+    "modifiers": {
+        "evaluation": {
+            "unknownFieldsAs": True
         }
     }
+}
 
-    # update policy
-    policy_update = itm_client.update_policy(policy_id, data)
-    if policy_update['_status']['status'] != 200:
-        err_msg = policy_update['_status']['status']
-        print(f'Error during policy update: {err_msg}')
+# update the policy object
+agentpolicy.match = match
 
-elif args.action == 'overwrite':
-    # modify the policy data previously pulled
-    policy['data'][0]['policy']['match']['simple']['rules'][0]['user.username'] = usernames
-    
-    # reset policy data to overwrite
-    policy_update = itm_client.overwrite_policy(policy_id, policy['data'][0])
-    if policy_update['_status']['status'] != 200:
-        msg = policy_update['_status']['message']
-        detail = policy_update['_status']['params']['details'][0]['message']
-        err_msg = f'{msg}: {detail}'
-        print(f'{err_msg}')
+# push to API
+resp = itm_client.update_agent_policy(target_policy_id, agentpolicy)
+
+if resp['_status']['status'] == '200':
+    print('Updated AgentPolicy successfully')
 
