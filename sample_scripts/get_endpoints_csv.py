b39b43561a68b61041d2317dda0fdde337d1379b
drizzo-tech <michael.olden@oldendigital.com>
2023-09-26 22:05:22 -0700

    updated get_endpoints_csv sample

--- sample_scripts/get_endpoints_csv.py
+++ sample_scripts/get_endpoints_csv.py
@@ -3,20 +3,46 @@ import json
 from proofpoint_itm import ITMClient
 
 # read in the config settings
-with open('settings.json', 'r') as f:
+with open('./dev/settings.json', 'r') as f:
     settings = json.loads(f.read())
 
 
 itm_client = ITMClient(settings)
 
-attrs = '*'
-instances = itm_client.get_endpoints(includes=attrs)
+instances = itm_client.get_endpoints()
 
+
+fields = {
+"id": "component.id",
+"hostname": "endpoint.hostname",
+"type": "component.kind",
+"realm": "component.realm",
+"region": "component.region",
+"status": "component.status.code",
+"version": "component.version",
+"os.type": "endpoint.os.kind",
+"os.name": "endpoint.os.name",
+"os.version": "endpoint.os.version",
+"time": "event.observedAt"
+}
+
+def get_value(obj, path):
+    keys = path.split(".")
+    key = keys.pop(0)
+    if key in obj:
+        if len(keys) == 0:
+            return obj[key]
+        else:
+            return get_value(obj[key], ".".join(keys))
+    else:
+        return ""
 
 if len(instances) > 0:
    with open('endpoints.csv', 'w', newline='', encoding='utf-8') as csvfile:
-       csvwriter = csv.DictWriter(csvfile, fieldnames=instances[0].keys())
-       csvwriter.writeheader()
+       csvwriter = csv.writer(csvfile)
+       csvwriter.writerow([field for field in fields.keys()])
        for instance in instances:
-           csvwriter.writerow(instance)
+           csvwriter.writerow([
+                get_value(instance, fields[field]) for field in fields.keys()
+           ])
 
